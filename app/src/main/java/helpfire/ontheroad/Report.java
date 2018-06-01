c77bb83eaa5a55fcd3b3b425cd4c80936d3a43f4
ftassatone <f.tassatone@studenti.unisa.it>
2018-06-01 17:51:23 +0200

    Creata interfaccia per ricezione segnalazione e compila report

---
+++ app/src/main/java/helpfire/ontheroad/Report.java
@@ -0,0 +1,9 @@
+package helpfire.ontheroad;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LenovoZ70 on 01/06/2018.
+ */
+public class Report extends AppCompatActivity {
+}

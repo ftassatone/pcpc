1b615893323cae9c49792a9504687e669218e77a
ftassatone <f.tassatone@studenti.unisa.it>
2018-06-04 14:14:13 +0200

    prova implementazione

--- app/src/main/java/helpfire/ontheroad/MainActivity.java
+++ app/src/main/java/helpfire/ontheroad/MainActivity.java
@@ -1,19 +1,49 @@
 package helpfire.ontheroad;
 
-import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v4.app.TaskStackBuilder;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout linearRinforzi, linearEmergenza;
+    private Button btRinforzi, btPolizia, btForestale, btCarabinieri, btAmbulanza;
+    private TextView nomeSegnalatoreTxt, indirizzoEmergenzaTxt, tipoEmergenzaTxt, cognomeSegnalatoreTxt, provinciaTxt, gradoEmergenzaTxt,
+            informazioniAggiuntiveTxt;
+    private Emergenza em;
+    private ArrayList<Emergenza> emergenze;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearRinforzi = (LinearLayout) findViewById(R.id.linearRinforzi);
+        linearEmergenza = (LinearLayout) findViewById(R.id.linearEmergenza);
+
+        btRinforzi = (Button) findViewById(R.id.btRinforzi);
+        btPolizia = (Button) findViewById(R.id.btPolizia);
+        btForestale = (Button) findViewById(R.id.btForestale);
+        btCarabinieri = (Button) findViewById(R.id.btCarabinieri);
+        btAmbulanza = (Button) findViewById(R.id.btAmbulanza);
+
+        nomeSegnalatoreTxt = (TextView) findViewById(R.id.nomeSegnalatoreTxt);
+        indirizzoEmergenzaTxt = (TextView) findViewById(R.id.indirizzoEmergenzaTxt);
+        tipoEmergenzaTxt = (TextView) findViewById(R.id.tipoEmergenzaTxt);
+        cognomeSegnalatoreTxt = (TextView) findViewById(R.id.cognomeSegnalatoreTxt);
+        provinciaTxt = (TextView) findViewById(R.id.provinciaTxt);
+        gradoEmergenzaTxt = (TextView) findViewById(R.id.gradoEmergenzaTxt);
+        informazioniAggiuntiveTxt = (TextView) findViewById(R.id.informazioniAggiuntiveTxt);
+
+        em = new Emergenza();
     }
 
     @Override
@@ -22,23 +52,5 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        int id = item.getItemId();
-
-        if(id == android.R.id.home){
-            Intent vaiReport = NavUtils.getParentActivityIntent(this);
-            /*vaiReport.putExtra("Materia", HomeClasse.materia);
-            vaiReport.putExtra("Classe", HomeClasse.classe);*/
-            if (NavUtils.shouldUpRecreateTask(this, vaiReport)) {
-                TaskStackBuilder.create(this).addNextIntentWithParentStack(vaiReport).startActivities();
-            } else {
-                NavUtils.navigateUpTo(this, vaiReport);
-            }
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
